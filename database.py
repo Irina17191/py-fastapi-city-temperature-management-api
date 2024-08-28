272ecc485371f45417df9e0af5c4ab41af36d1fb
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-08-28 20:41:37 +0300

    Add database.py

---
+++ database.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL: str | None = "sqlite:///./city_temperature_api.db"
+
+engine = create_async_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, class_=AsyncSession
+)
+
+Base = declarative_base()
+
+async def get_db() -> AsyncSession:
+    async with SessionLocal() as session:
+        yield session
