7cb2fcc0a8b312f4924a467a71be2c6fafd48031
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-08-31 17:31:26 +0300

    Update settings.py database.py

--- database.py
+++ database.py
@@ -1,15 +1,15 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL: str | None = "sqlite:///./city_temperature_api.db"
+DATABASE_URL = "sqlite+aiosqlite:///./city_temperature_api.db"
 
 engine = create_async_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(
+SessionLocal = async_sessionmaker(
     autocommit=False, autoflush=False, bind=engine, class_=AsyncSession
 )
 
--- settings.py
+++ settings.py
@@ -2,9 +2,10 @@ from pydantic.v1 import BaseSettings
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "FastAPI in Details"
+    PROJECT_NAME: str = "City Temperature Management API"
 
-    DATABASE_URL: str | None = "sqlite:///./city_temperature_api.db"
+    DATABASE_URL: str = "sqlite:///./city_temperature_api.db"
+    # DATABASE_URL = "sqlite+aiosqlite:///./city_temperature_api.db"
 
     class Config:
         case_sensitive = True
